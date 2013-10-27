1fcf462d8a52d5052c918556316e3cc66b7f9c66
Rob Lifford <rob@lifford.org>
2013-10-27 09:06:51 -0500

    add bump command to main window

--- hhb.js
+++ hhb.js
@@ -58,7 +58,7 @@ function showCapture(x, y, pointVal) {
 
 function Pig(x, y, imgNum) {
 	this.x	= x;
-	this.y	= y;				
+	this.y	= y;
 	this.imgNum	= imgNum;
 	this.xVel =	this.yVel = 0;
 	this.pigImg = new Image();
@@ -103,15 +103,15 @@ function Bird(x, y, color, axis, direction, id, imgSrc, captureX, captureY, repo
 		self.launched = true;
 		var from = { anim: self.startPoint };
 		var to = { anim:  self.endPoint  };
-		$(from).animate(to, { 
+		$(from).animate(to, {
 			duration: 125,
 			step: function() {
 				if (self.axis == 'x') self.x = this.anim;
 				else self.y = this.anim;
-				
+
 				if (self.radius < self.maxRadius) self.radius += 7;
 				else self.radius -= 7;
-				
+
 			},
 			complete: function() {
 				self.capture();
@@ -124,7 +124,7 @@ function Bird(x, y, color, axis, direction, id, imgSrc, captureX, captureY, repo
 		self.radius = self.defaultRadius;
 		var from = { anim: self.endPoint };
 		var to = { anim:  self.startPoint };
-		$(from).animate(to, { 
+		$(from).animate(to, {
 			duration: 275,
 			step: function() {
 				if (self.axis == 'x') self.x = this.anim;
@@ -134,16 +134,16 @@ function Bird(x, y, color, axis, direction, id, imgSrc, captureX, captureY, repo
 				(self.axis == 'x') ? self.x = self.startPoint : self.y = self.startPoint;
 				self.launched = false;
 				self.reportContext.clearRect(0,0,pfWidth,pfHeight);
-				
+
 			}
 		});
 	}
 	this.capture = function() {
-		
+
 		var r = this.reportContext;
 
 		for(i=0; i<pigCount; i++) {
-			
+
 			if (pigs[i].inPlay == false) continue;
 
 			if ( inCaptureArea(this.captureX, this.captureY, this.captureRadius, pigs[i].centerX, pigs[i].centerY) ) {
@@ -183,7 +183,7 @@ function victoryMessage(winner, loser) {
 			'awwww snap! <b>' + winner + '</b> schooled you, <b>' + loser + '</b>',
 			'<b>' + winner + "</b>: thrill of victory! &nbsp;★&nbsp; <b>" + loser + "</b>: agony of defeat.",
 			'epic win: <b>' + winner + '</b> &nbsp;★&nbsp; epic fail: <b>' + loser + '</b>'
-			
+
 	];
 	var index = Math.floor( Math.random()* messages.length);
 	return messages[index];
@@ -191,13 +191,13 @@ function victoryMessage(winner, loser) {
 
 
 function gameOver() {
-	
+
 	gameState = gameStates.isOver;
 	eastReport.clearRect(0,0,pfWidth,pfHeight);
 	westReport.clearRect(0,0,pfWidth,pfHeight);
 	clearInterval(intervalId);
 	unbindGameplayHandlers();
-			
+
 	var eScore = parseFloat($('#eScore').text());
 	var wScore = parseFloat($('#wScore').text());
 	var message;
@@ -210,7 +210,7 @@ function gameOver() {
 	else {
 		message = 'it\s a tie! REMATCH!';
 	}
-	
+
 	$('#mask').removeClass('offscreen');
 	$('#victor').html(message);
 	var victorOffset = $('#victor').outerWidth() / 2;
@@ -222,11 +222,11 @@ function gameOver() {
 function drawCaptureArea(bird) {
 
 	staticCanvas.fillStyle = 'rgba(255,255,255, 0.8)';
-	staticCanvas.beginPath();	
+	staticCanvas.beginPath();
  	staticCanvas.arc(bird.captureX, bird.captureY, bird.captureRadius, 0, Math.PI*2, false);
 	staticCanvas.closePath();
 	staticCanvas.fill();
-	
+
 }
 
 function drawPig(pig) {
@@ -237,7 +237,7 @@ function drawPig(pig) {
 	}
 	if (pig.y + pig.yVel < 0 || pig.y + pig.yVel + pig.height > pfHeight ) {
 		pig.yVel = -pig.yVel
-	}					
+	}
 	pigCanvas.drawImage(pig.pigImg, pig.x += pig.xVel, pig.y += pig.yVel );
 }
 
@@ -270,7 +270,7 @@ function bindGameplayHandlers() {
 				break;
 			case 13: // enter
 				e.preventDefault();
-				setPigSpeedsAndDirections();			
+				setPigSpeedsAndDirections();
 			default:
 				break;
 		}
@@ -297,12 +297,12 @@ function startOrContinue() {
 	bindGameplayHandlers();
 	$('#start').text('pause');
 	intervalId = setInterval(draw, 1000/fps);
-	
+
 	// send a random keydown (t) to the playfield to make sure it's focused to get events from Kinect
 	var e = jQuery.Event('keydown');
 	e.keyCode = 84;
 	$(document).trigger(e)
-	
+
 	return intervalId;
 
 }
@@ -318,66 +318,66 @@ function pausePlay() {
 function setPigSpeedsAndDirections() {
 
 	// to make sure there aren't any pigs that are too slow but keep a nice variety of directions
-	
+
 	for(i=0; i<pigCount; i++) {
 
 		if (!(pigs[i].inPlay)) continue;
-		
+
 		pigs[i].xVel = pigVelocityRandomizer();
 		pigs[i].yVel = pigVelocityRandomizer();
-				
+
 		var totalVelocity = Math.abs(pigs[i].xVel) + Math.abs(pigs[i].yVel);
-		
+
 		if (totalVelocity < velocityThreshold ) {
-			
-			var deficit = velocityThreshold - totalVelocity;  
-					
+
+			var deficit = velocityThreshold - totalVelocity;
+
 			var switcher = startDirectionRandomizer();
 			if (switcher > 0) {
 				pigs[i].xVel = (pigs[i].xVel > 0) ? pigs[i].xVel + deficit : pigs[i].xVel - deficit;
 			}
 			else {
-				pigs[i].yVel = (pigs[i].yVel > 0) ? pigs[i].yVel + deficit : pigs[i].yVel - deficit; 
+				pigs[i].yVel = (pigs[i].yVel > 0) ? pigs[i].yVel + deficit : pigs[i].yVel - deficit;
 			}
-			//console.log('final total velocity:'+ (Math.abs(pigs[i].xVel) + Math.abs(pigs[i].yVel)) );	
+			//console.log('final total velocity:'+ (Math.abs(pigs[i].xVel) + Math.abs(pigs[i].yVel)) );
 		}
 	}
 }
 
 function init() {
-	
+
 	$('#victor').fadeOut('fast', function() { $(this).css({visibility:'visible'}); } );
-	
-	if(!document.createElement('canvas').getContext) 
+
+	if(!document.createElement('canvas').getContext)
 	{
 		$(body).empty().html('<p>This browser doesn\'t support the canvas element. Try <a href="http://google.com/chrome">one</a> <a href="http://getfirefox.com">that</a> <a href="http://ie.microsoft.com/testdrive/">does</a>.');
 	}
-	
+
 	if (intervalId != undefined) clearInterval(intervalId);
-	
+
 	pigs = [];
 	birds = [];
 	capturedPigs = [];
-	
+
 	birdCanvas = document.getElementById('hhb').getContext('2d');
 
 	birdCanvas.shadowColor = '#555555';
 	birdCanvas.shadowOffsetY = 2;
-	birdCanvas.shadowBlur = 8;	
+	birdCanvas.shadowBlur = 8;
 
 	pigCanvas = document.getElementById('pigs').getContext('2d');
 	birdFacesCanvas = document.getElementById('birdFaces').getContext('2d');
 	staticCanvas = document.getElementById('static').getContext('2d');
 	eastReport = document.getElementById('eastReport').getContext('2d');
 	westReport = document.getElementById('westReport').getContext('2d');
-	
+
 	birdCanvas.clearRect(0, 0, pfWidth, pfHeight);
 	pigCanvas.clearRect(0, 0, pfWidth, pfHeight);
 	birdFacesCanvas.clearRect(0, 0, pfWidth, pfHeight);
 	eastReport.clearRect(0, 0, pfWidth, pfHeight);
 	westReport.clearRect(0, 0, pfWidth, pfHeight);
 	staticCanvas.clearRect(0, 0, pfWidth, pfHeight);
-					
+
 	// generate pigs w/ randomized start position & imgNum
 	for(i=0; i<pigCount; i++) {
 
@@ -385,9 +385,9 @@ function init() {
 					Math.floor(Math.random()*pigStartRadius) + pigStartXOffset, // start X
 					Math.floor(Math.random()*pigStartRadius) + pigStartYOffset, // start Y
 					Math.floor(Math.random()*6) +  1 // randomize the imgNum
-				)	  
-			);						
-	
+				)
+			);
+
 		switch(pigs[i].imgNum) {
 			case 1:
 				pigs[i].pigImg.src = pigImgs.pig1;
@@ -413,20 +413,20 @@ function init() {
 			default:
 				break;
 		}
-		
+
 		pigs[i].width = pigs[i].pigImg.width;
 		pigs[i].height = pigs[i].pigImg.height;
-		
+
 		pigs[i].imgOffsetX = pigs[i].width / 2;
-		pigs[i].imgOffsetY = pigs[i].height / 2; 
+		pigs[i].imgOffsetY = pigs[i].height / 2;
 	}
-	
+
 	// reusable function to set or change pigs' xVel and yVel;
 	setPigSpeedsAndDirections();
-		
+
 	eastBird 	= new Bird( pfWidth, pfHeight/2, '#64acc8', 'x', -1, 'e', birdImgs.blueBird,  pfWidth-captureOffset, pfHeight/2, eastReport );
 	westBird 	= new Bird(   0, pfHeight/2, '#000000', 'x',  1, 'w', birdImgs.blackBird, captureOffset, pfHeight/2, westReport );
-	
+
 	birds = [ eastBird, westBird ];
 
 	for(i=0; i<birdCount; i++)
@@ -448,62 +448,66 @@ function init() {
 		}
 		drawBird(birds[i]);
 	}
-	
+
 	$('#eScore, #wScore').text('0');
-	$('#victor').empty();	
+	$('#victor').empty();
 	$('#mask').removeClass('offscreen');
 	$('#start').focus();
-		
+
 	draw();
 	gameState = gameStates.isInitialized;
 
 }
-			
+
 function draw() {
 
 	birdCanvas.clearRect(0, 0, pfWidth, pfHeight);
 	pigCanvas.clearRect(0, 0, pfWidth, pfHeight);
 	birdFacesCanvas.clearRect(0, 0, pfWidth, pfHeight);
-	
+
 	for(i=0; i<pigCount; i++)
 	{
 		if (pigs[i].inPlay == true) {
 			pigs[i].centerX = pigs[i].x + pigs[i].imgOffsetX;
 			pigs[i].centerY = pigs[i].y	 + pigs[i].imgOffsetY;
 			drawPig(pigs[i]);
-		} 
+		}
 	}
-	
+
 	for(i=0; i<birdCount; i++)
 	{
 		drawBird(birds[i]);
 	}
-	
+
 	// for debugging... you can log something in draw()
 	// without killing the console via if(firstDraw)
 	// how the hell do real game-loop developers do this?
 	//firstDraw = false;
-  	
+
 }
 
 
 
 function bindPersistentControls() {
 	$(document).bind('keydown', function(e) {
-		if(e.keyCode == 32) { // space bar
+		if(e.keyCode === 32) { // space bar
 			e.preventDefault();
 			$('#start').click();
 		}
-		if(e.keyCode == 77) { // m is for music
+		if(e.keyCode === 77) { // m is for music
 			e.preventDefault();
 			$('#song').toggleClass('hidden');
 		}
-		if(e.keyCode == 80) {
+		if(e.keyCode === 80) {
 			e.preventDefault();
 			toggleMusicPlayback(); // p is for play/pause
 		}
+		if(e.keyCode === 66) {
+			e.preventDefault(); // b is for bump
+			setPigSpeedsAndDirections();
+		}
 	});
-	
+
 	$('#start').click(function() {
 		if (gameState == gameStates.isPlaying) {
 			pausePlay();
@@ -514,7 +518,7 @@ function bindPersistentControls() {
 		}
 		else {
 			startOrContinue();
-		}	
+		}
 	});
 }
 
@@ -539,7 +543,7 @@ $(document).ready(function(){
 
 	var client = new SocketClient(exports.CLIENT_TYPE_BOARD, exports.SOCKET_ADDRESS);
 	client.listen(function(msg){
-	    
+
 	    switch (msg.message) {
 	    	case 'startOrContinue':
 	    		$('#start').click();
@@ -553,9 +557,9 @@ $(document).ready(function(){
 	    	default:
 	    		break;
 	    }
-	    
+
 	});
-	
+
 });
 
 // using window.load instead of document.ready to make sure all the external and dataURL images are loaded
